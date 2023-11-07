cc62e4e8a7e642242b2390608656a6af98030df8
suvarnasandeep <poojary.udupi@gmail.com>
2023-11-07 20:46:16 +0530

    utils

---
+++ src/DataStructureAlgorithm/Utils.java
@@ -0,0 +1,33 @@
+package DataStructureAlgorithm;
+
+public class Utils {
+        public static void printArray(int[] arr) {
+        System.out.print("[ ");
+        for(int i : arr)
+            System.out.print(i + " ");
+
+        System.out.println("]");
+        //System.out.println("------------------------");
+    }
+
+        public static void printArray(int[] arr1, int[] arr2) {
+        System.out.println("------------------------");
+        System.out.print("[ ");
+        for(int i : arr1)
+            System.out.print(i + " ");
+
+        System.out.println("]");
+        System.out.print("[ ");
+        for(int i : arr2)
+            System.out.print(i + " ");
+
+        System.out.println("]");
+
+
+    }
+
+        public static void setSeparator(String msg) {
+        System.out.println("************" + msg +"*****************");
+    }
+}
+
